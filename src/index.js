863bf60549f5aa954e1067b53d55466add9d2658
HoanNam <m8mi0909@gmail.com>
2025-08-02 23:41:10 +0700

    Static file & SCSS

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const { engine: handlebars } = require('express-handlebars'); // Import Handleba
 const app = express(); // Tạo instance ứng dụng
 const port = 3127; // Cổng server
 
+// Cấu hình sử dụng file tĩnh
+app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({ extname: '.hbs' })); // Định nghĩa template engine
+app.engine('hbs', handlebars({ extname: '.hbs', defaultLayout: 'main' })); // Định nghĩa template engine
 app.set('view engine', 'hbs'); // Sử dụng template engine
 app.set('views', path.join(__dirname, 'resources/views')); // Cấu hình đường dẫn cho thư mục views
 
